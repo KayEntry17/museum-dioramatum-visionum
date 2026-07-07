8d9f5f6e49090796b444085298a964eae9a56e6a
Kay <150719310+KayEntry17@users.noreply.github.com>
2026-07-07 17:15:33 +0100

    added to main scene. adjusted cameras. moving on to settings menu

--- ui/sub_viewport.gd
+++ ui/sub_viewport.gd
@@ -3,11 +3,16 @@ extends SubViewport
 func _ready():
 	get_tree().root.size_changed.connect(on_viewport_size_changed)
 	adjust()
+	#print($"../TextureRect".size)k
 
 
 func on_viewport_size_changed():
 	adjust()
+	
 # Called when the node enters the scene tree for the first time
 func adjust():
 	self.size=DisplayServer.window_get_size()
+	self.size.x=$"../TextureRect".size.x/$"../TextureRect".size.y*self.size.y
 	print(self.size)
+	print("m8ey")
+	print(str($"../TextureRect".size)+"arghhhh")
