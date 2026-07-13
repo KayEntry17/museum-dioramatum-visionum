2d4778e912c6943b3edc9f63a9d83f0b5ddc9e65
Kay <150719310+KayEntry17@users.noreply.github.com>
2026-07-13 13:05:45 +0100

    set up animated wire materials and began setting up the intro cutscene

---
+++ museum/wire.gd
@@ -0,0 +1,17 @@
+extends MeshInstance3D
+var on:bool=true
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	turnoff()
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+func turnon():
+	var tween = get_tree().create_tween()
+	tween.tween_property(self.get_surface_override_material(0), "shader_parameter/on",4.0, 2)
+func turnoff():
+	var tween = get_tree().create_tween()
+	tween.tween_property(self.get_surface_override_material(0), "shader_parameter/on",0.0, 2)
