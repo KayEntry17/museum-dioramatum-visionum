fde6ad57e814be5b4439806a3833961859584a8a
Kay <150719310+KayEntry17@users.noreply.github.com>
2026-07-13 16:02:13 +0100

    added animation to the other doors, adjusted lighting

--- museum/door.gd
+++ museum/door.gd
@@ -2,6 +2,7 @@
 extends MeshInstance3D
 @export var on:bool
 @export var speed:float=1.0
+@export var modifier:int=1
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -9,10 +10,13 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	if on:
-		self.rotation_degrees.y=lerp(self.rotation_degrees.y,130.0,speed*delta)
+	if !on:
+		self.rotation_degrees.y=lerp(self.rotation_degrees.y,0.0+(modifier-1)*180.0,speed*delta)
 	else:
-		self.rotation_degrees.y=lerp(self.rotation_degrees.y,0.0,speed*delta)
+		if modifier==2:
+			self.rotation_degrees.y=lerp(self.rotation_degrees.y,50.0,speed*delta)
+		else:
+			self.rotation_degrees.y=lerp(self.rotation_degrees.y,130.0,speed*delta)
 func turn_on():
 	on=true
 func turn_off():
