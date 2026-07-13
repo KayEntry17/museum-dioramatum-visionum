60e1893a16519a4a5d43f4d21a7397c37d762359
Kay <150719310+KayEntry17@users.noreply.github.com>
2026-07-13 17:49:33 +0100

    animation and timing adjustments for the intro

--- museum/door.gd
+++ museum/door.gd
@@ -6,9 +6,6 @@ extends MeshInstance3D
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	if !on:
 		self.rotation_degrees.y=lerp(self.rotation_degrees.y,0.0+(modifier-1)*180.0,speed*delta)
