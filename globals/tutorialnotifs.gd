e4367362026d7c3e44e2fa28dd136dadc9f561ef
Kay <150719310+KayEntry17@users.noreply.github.com>
2026-07-14 17:39:05 +0100

    finished the pop up system along with a queue for unshown notifs

--- globals/tutorialnotifs.gd
+++ globals/tutorialnotifs.gd
@@ -1,6 +1,7 @@
 extends Control
-var on:bool
-
+var on:bool=false
+var off:bool=true
+var notiflist:Array[Array]
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -9,9 +10,21 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
-func do_it(text,time):
+func do_it(text,time): 
 	if !on:
+		on=true
 		$ColorRect/Label.text=text
 		$AnimationPlayer.play("appear")
 		await get_tree().create_timer(time).timeout
+		$AnimationPlayer.play_backwards("appear")
+		off=false
+	else:
+		notiflist.append([text,time])
+func end():
+	if !off:
+		off=true
+		on=false
+		if notiflist!=[]:
+			do_it(notiflist[0][0],notiflist[0][1])
+			notiflist.pop_at(0)
 	
---
+++ museum/notif.gd
@@ -0,0 +1,21 @@
+extends Area3D
+@export var text:String
+@export var time:=1.5
+
+var used=false
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	used=false
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_body_entered(body: Node3D) -> void:
+	#print(used)
+	if !used:
+		
+		if body.is_in_group("player"):
+			Tut.do_it(text,time)
+			used=true
