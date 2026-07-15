88baa79058df14f4e279cadae254d49ceda2816d
Kay <150719310+KayEntry17@users.noreply.github.com>
2026-07-15 19:15:37 +0100

    level 3 and moving platforms

---
+++ levels/movingbox.gd
@@ -0,0 +1,20 @@
+#@tool
+extends Node3D
+@export var direction:Vector3
+@export var distance:float
+@export var interval:float=1.0
+var basepos:Vector3
+var progress: float=0
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	basepos=position
+
+
+ #Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	#print("babababa")
+	#progress=0
+	progress+=delta/interval
+	progress=progress-int(progress)
+	position=basepos+distance*direction*sin(PI*progress*2)
+	#print(progress)
